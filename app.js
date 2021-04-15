c74b926fdf1cc7b98d7f0e1f4c46e69f9afd4644
Umut Akin <umutakin@fastmail.com>
2021-04-15 11:00:46 +0300

    node 07 - view engines

--- app.js
+++ app.js
@@ -10,26 +10,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index', { title: 'Home'});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit amet consectetur'}
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    //res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about');
+    res.render('about', { title: 'About'});
 });
 
-// // redirects
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// });
-
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a new Blog'});
 });
 
 // 404 page
 app.use((req, res) => {
-    //res.status(404).sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404'});
 });
