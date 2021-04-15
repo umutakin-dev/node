93d8f324f1ba89a765f78d45f0e3c0e46b20ee12
Umut Akin <umutakin@fastmail.com>
2021-04-15 19:21:21 +0300

    node 10 - get, post & delete requests

--- app.js
+++ app.js
@@ -18,11 +18,9 @@ mongoose
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-// app.listen(3000);
-
 // middlaware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -38,7 +36,6 @@ app.get('/blogs', (req, res) => {
     Blog.find()
         .sort( {createdAt: -1 })
         .then(result => {
-            //res.send(result);
             res.render('index', { title: 'All Blogs', blogs: result });
         })
         .catch(err => {
@@ -46,8 +43,40 @@ app.get('/blogs', (req, res) => {
         })
 })
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs')
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
 app.get("/blogs/create", (req, res) => {
     res.render("create", { title: "Create a new Blog" });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details' });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch(err => console.log(err));
 });
 
 // 404 page
